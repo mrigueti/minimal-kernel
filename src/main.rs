5d3dd9acf007f91d2c010b7016b7d61b640b9b08
Maykel <maykelrigueti93@gmail.com>
2025-04-14 03:06:06 -0300

    added message at boot

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,19 @@ fn panic(_info: &PanicInfo) -> ! {
 /// Esta é a função de entrada do kernel.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    print_message();
+    ascii_image();
     loop {}
 }
 
+fn print_message() {
+    let vga_buffer = 0xb8000 as *mut u8;
+    let message = b"Hello, New kernel!";
+
+    for (i, &byte) in message.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte; // caractere
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x0f; // cor do caractere
+        }
+    }
+}
