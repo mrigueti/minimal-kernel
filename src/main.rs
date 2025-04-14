805feabed8ed257a734a94cea7a41b71c49697ce
Maykel <maykelrigueti93@gmail.com>
2025-04-14 03:06:23 -0300

    added ascii art

--- src/main.rs
+++ src/main.rs
@@ -28,3 +28,36 @@ fn print_message() {
         }
     }
 }
+
+const VGA_WIDTH: usize = 80;
+
+fn ascii_image() {
+    let image = r#"
+        .--.
+       |o_o |
+       |:_/ |
+      //   \ \
+     (|     | )
+    /'\_   _/`\
+    \___)=(___/
+    "#;
+
+    let vga_buffer = 0xb8000 as *mut u8;
+    let start_row = 5; // define posição inicial da linha
+    let start_col = 10; // define posição inicial da coluna
+    let mut current_row = start_row; // inicializa a linha atual
+    let mut current_col = start_col; // inicializa a coluna atual
+
+    for byte in image.bytes() {
+        if byte == b'\n' {
+            current_row += 1;
+            current_col = start_col;
+        } else {
+            unsafe {
+                *vga_buffer.offset((current_row * VGA_WIDTH + current_col) as isize * 2) = byte; // caractere
+                *vga_buffer.offset((current_row * VGA_WIDTH + current_col) as isize * 2 + 1) = 0x0f; // cor do caractere
+            }
+            current_col += 1;
+        }
+    }
+}
